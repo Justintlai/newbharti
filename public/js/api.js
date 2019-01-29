cbffec9b888fdf5390a7e8abd9bdd0f0f0ea8b11
Justin <justin.t.lai@hotmail.co.uk>
2019-01-29 08:05:55 +0000

    update design and filters

--- public/js/api.js
+++ public/js/api.js
@@ -1,32 +1,121 @@
 $(document).ready(function () {
 
-  $("#posts").on("click", function (e) {
-    e.preventDefault();
-    console.log("Post clicked")
-    $.ajax({
-      url: "/posts",
-      type: "GET",
-      dataType: "json",
-      success: function (result) {
-        console.log(result);
-        var data = "";
-        $.each(result, function (i, item) {
-          data += `
-          <div>
-          <h1>${item.fields.Posts}</h1>
-            <p>${item.fields['Number of Posts']}</p>
-          
-          </div>
-          `
-        });
-        $(".data").html(data);
-      },
-      error: function (data) {
-        console.log("ERROR DATA: ", data);
 
+});
+
+document.querySelector(".btn-close").onclick = function (e) {
+  e.preventDefault();
+  //hide table
+  $("#post-table").DataTable().destroy();
+  $("#post-table").addClass("hidden");
+  //hide button
+  $(".btn-close").addClass("hidden");
+  $("#btn-posts").html("View Posts")
+  //hide filters
+  $("#filters").addClass("hidden");
+
+};
+
+
+document.getElementById("btn-posts").onclick = function (e) {
+  e.preventDefault();
+  console.log("Clicked");
+
+  //show table
+  $("#post-table").removeClass("hidden");
+  $("#filters").removeClass("hidden");
+  $(".btn-close").removeClass("hidden");
+
+  $("#post-table").DataTable({
+    dom: "<'top'Bfl>rtip",
+    ajax: "http://localhost:4000/posts",
+    initComplete: function () {
+      $(".filters").empty();
+      this.api().columns([0, 1, 2, 8, 9, 10]).every(function (i) {
+        var column = this;
+
+        var filterName = $('#post-table').DataTable().column(i).title();
+
+        $(`<div class="filters__wrapper-${i}">
+          <label>${filterName}</label>
+        </div>`).appendTo(".filters");
+
+        //create the select with event handler on change
+        var select = $('<select class="sel sel__${i}"><option value=""></option></select>')
+          .appendTo($(`.filters__wrapper-${i}`))
+          .on('change', function () {
+            var val = $.fn.dataTable.util.escapeRegex(
+              $(this).val()
+            );
+            column
+              .search(val, true, false)
+              .draw();
+          });
+        //fill the data in the column
+        column.data().unique().sort().each(function (d, j) {
+          select.append(`<option class='filters__item' value=${d}>${d}</option>`)
+        });
+        //remove duplicate entries
+        var map = {};
+        $("select option").each(function () {
+          if (map[this.value] && this.value !== "") {
+            $(this).remove()
+          }
+          map[this.value] = true
+        });
+      })
+    },
+    destroy: true,
+    autoWidth: true,
+    fixedHeader: true,
+    paging: true,
+    search: {
+      smart: false
+    },
+    responsive: true,
+    searching: true,
+    scrollX: true,
+    scrollCollapse: true,
+    buttons: [
+      {
+        text: 'Reset Filters',
+        attr: {
+          id: 'reset'
+        },
+        className: 'uk-button uk-button-secondary btn-reset',
+        action: function (e, dt, node, config) {
+          e.preventDefault();
+
+          //get all the select dropdowns
+          var filters = document.querySelectorAll(".sel");
+          //iterate through each one and reset the value
+          for (var i = 0; i < filters.length; i++) {
+            console.log(filters.val());
+            $(`.sel__${i}`).val('');
+          }
+          //set the back to nothing
+          $("#post-table").DataTable().search("").columns().search('').draw();
+        }
       }
-    });
+    ],
+    columns: [
+      { data: "All Posts", defaultContent: "" },
+      { data: "Vacancy Name", defaultContent: "" },
+      { data: "Location Lookup", defaultContent: "" },
+      { data: "Number of Posts", defaultContent: "" },
+      { data: "Total Number Lookup", defaultContent: "" },
+      { data: "Salary Range 1", defaultContent: "" },
+      { data: "Salary Range 2", defaultContent: "" },
+      { data: "Salary (extra info)", defaultContent: "" },
+      { data: "Male or Female Lookup", defaultContent: "" },
+      { data: "Upper Date Lookup", defaultContent: "" },
+      { data: "Lower Date Lookup", defaultContent: "" },
+      { data: "Department Lookup", defaultContent: "" },
+      { data: "Notification Date Lookup", defaultContent: "" }
+    ]
   });
 
 
-})
+  //change button name
+  $("#btn-posts").html("Reload Data");
+}
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
   apiKey: process.env.AIRTABLE_API_KEY
 });
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appdaFHqyWFcRWHpj');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appxoBTFr9eZvYgDh');
 
 //assign home route
 router.get("/", async (req, res) => {
@@ -13,22 +13,144 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/posts", async (req, res) => {
-  data = [];
-  base('Posts').select({
-    // fields: ['Selection Test', 'Salary Range 1'],
-    // filterByFormula:
-    maxRecords: 3,
-    view: 'Grid view'
-  }).firstPage(function (err, records) {
+  let postData = [];
+  base('All Posts').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 20,
+    view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+    records.forEach(function (record, i) {
+      // console.log('Retrieved', record.get('All Posts'));
+      postData[i] = { ...record.fields };
+    });
+
+    console.log("postData ", postData);
+    res.json({ data: postData });
+    // // To fetch the next page of records, call `fetchNextPage`.
+    // // If there are more records, `page` will get called again.
+    // // If there are no more records, `done` will get called.
+    // fetchNextPage();
+
+  }, function done(err) {
     if (err) { console.error(err); return; }
-    res.json(records);
-    // records.forEach(function (record, i) {
-    //   console.log('Retrieved', record.get('Posts'));
-    //   data.push(record.get('Vacancy Name'));
-    // });
-    // res.json({ data: data })
   });
+
+
+
+  // base('All Posts').select({
+  //   // fields: ['Selection Test', 'Salary Range 1'],
+  //   maxRecords: 3,
+  //   view: 'Grid view'
+  // }).firstPage(function (err, vacancies) {
+  //   if (err) { console.error(err); return; }
+  //   // res.json(vacancies);
+
+  //   //allocate vacancies to data
+  //   // vacancyData = { ...vacancies };
+
+  //   const promise = new Promise((resolve, reject) => {
+  //     vacancies.forEach(function (vacancy, i) {
+  //       //add to the currency vacancyData object
+  //       vacancyData.push({ ...vacancy.fields });
+  //       resolve("Success");
+  //     });
+  //   });
+
+  //   // const promise2 = new Promise((resolve, reject) => {
+  //   //   //loop through each vacancy
+  //   //   vacancyData.forEach(function (vacancy, i) {
+  //   //     //use the id and get the post details
+  //   //     console.log("Vacancy Post ", vacancy["All Posts"]);
+
+  //   //     //loop through each linked post record
+  //   //     vacancyData.Posts = getPostByID(vacancy["All Posts"])
+  //   //     console.log("VACANCY POSTS ", vacancyData.Posts);
+  //   //     // console.log("vacancyData ", vacancyData[i]["All Posts"]);
+  //   //     resolve("SUCCESS")
+  //   //     // vacancy.Posts.forEach(function (post, j) {
+  //   //     //   //get the post linked information 
+  //   //     //   base("Posts").find(post, function (err, postFound) {
+  //   //     //     // console.log("Post Found ", i, postFound.fields);
+  //   //     //     vacancyData.Posts = postFound.fields;
+  //   //     //     resolve("Success");
+  //   //     //   });
+  //   //     // })
+  //   //   });
+  //   // })
+
+  //   res.json({ data: vacancyData })
+
+
+
+
+  //   // await vacancies.forEach(async function (vacancy, x) {
+  //   //   //selects a specific field from the Vacancy table
+  //   //   // console.log('Retrieved', vacancy.get('Posts'));
+
+  //   //   console.log(vacancy.fields["Vacancy Name"]);
+  //   //   // vacancyData.push({ ["Vacancy Name"]: vacancy.fields["Vacancy Name"] })
+
+  //   //   console.log(vacancy.fields["Posts"]);
+  //   //   await vacancy.fields["Posts"].forEach(function (post, i) {
+  //   //     console.log("POSTS  ", post)
+  //   //     //get the post information
+  //   //     base("Posts").find(post, function (err, result) {
+  //   //       console.log("POSTS RESULT ++++++++++++++++++++++++++++> ", i, result.fields.Posts);
+  //   //       console.log(" VACANCY ", vacancyData[x]);
+  //   //       // vacancyData[x].Posts = result.fields.Posts;
+  //   //       // console.log("VACANCY POST ", vacancyData[x].Posts)
+  //   //       let pair = { Posts: result.fields.Posts }
+  //   //       vacancyData[x] = { ...vacancyData[x], ...pair }
+  //   //     })
+  //   //   })
+  //   // });
+
+  // });
 });
+
+
+//return post information
+getPostByID = function (postArray) {
+  return new Promise(function (resolve, reject) {
+    base('All Posts').select({
+      filterByFormula: filterBy(postArray),
+      maxRecords: postArray.length,
+      view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function (record) {
+        console.log('Retrieved', record.get('Posts'));
+        resolve(record);
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+    }, function done(err) {
+      if (err) { console.error(err); return; }
+    });
+  });
+}
+
+filterBy = function (recArray) {
+  return new Promise(function (resolve, reject) {
+    let filterString = '';
+    recArray.forEach((recordId, index) => {
+      filterString += (`RECORD_ID() = '${recordId}'`);
+      if (index < (recArray.length - 1)) {
+        filterString += (', ');
+      } else {
+        filterString += (')');
+      }
+    });
+    console.log("FILTER STRING ====> ", filterString);
+    resolve(filterString)
+  });
+}
 
 router.get("/logout", async (req, res) => {
   req.session.destroy(function (err) {
